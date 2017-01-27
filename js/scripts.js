c9198b8a0d013d530a94f253240991fa569ab0bf
Nicole Sanders <nsanders9022@gmail.com>
2017-01-27 08:47:00 -0800

    return the base price of  when a pizza is ordered regardless of the selections made

--- js/scripts.js
+++ js/scripts.js
@@ -3,12 +3,31 @@ function Pizza(toppings, size) {
   this.size = size;
 }
 
-
+Pizza.prototype.toppingsPrice = function () {
+  var price = 10;
+  // this.toppings.forEach(topping) {
+  //
+  // }
+  return price;
+}
 
 
 
 $(document).ready(function () {
-  ("form#pizza-order").submit(function(event) {
+  $("form#pizza-order").submit(function(event) {
     event.preventDefault();
+
+
+    var pizzaOrder = new Pizza(toppingsPriceAmount, "small")
+
+    var toppingsPriceAmount = pizzaOrder.toppingsPrice()
+    
+    // $("input:checkbox[name=toppings]:checked").each(function(){
+    //   var toppingSelected = $(this).val();
+    //   this.toppings.push(toppingsSelected);
+    //
+    // });
+
+    $("#total").text(pizzaOrder.toppingsPrice())
   })
 })
