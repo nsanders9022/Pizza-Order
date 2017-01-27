6eb625afdbc1408f618985d9eb8fbdf1eb72c196
Nicole Sanders <nsanders9022@gmail.com>
2017-01-27 10:20:26 -0800

    clear the beckboxes and radio buttons when the form is submitted. Add images to be displayed when their corresponding item is chosen - still need to fix bugs

--- js/scripts.js
+++ js/scripts.js
@@ -13,22 +13,62 @@ Pizza.prototype.toppingsPrice = function() {
 
 Pizza.prototype.sizePrice = function(size) {
   var priceForSize = 0;
-  if (this.size === "medium") {
-    priceForSize += 2;
-  } else if (this.size === "large") {
-    priceForSize += 4;
-  }
+    if (this.size === "medium") {
+      priceForSize += 2;
+    } else if (this.size === "large") {
+      priceForSize += 4;
+    }
   return priceForSize;
 }
 
 
 Pizza.prototype.totalPrice = function() {
   var price = 10 + this.toppingsPrice() + this.sizePrice();
+  return price;
+}
 
-  return price
+Pizza.prototype.resetForm = function() {
+  $('input[name=size]').prop('checked',false);
+  $('input[name=toppings]').prop('checked',false);
 }
 
 $(document).ready(function () {
+  $("#small").css('display', 'none');
+  $("#medium").css('display', 'none');
+  $("#large").css('display', 'none');
+
+  $("input:radio[name=size]").change(function() {
+    var sizeVal = $("input:radio[name=size]:checked").val();
+
+    if (sizeVal === "small") {
+      $("#small").css('display', 'block');
+    } else if (sizeVal === "medium") {
+      $("#medium").css('display', 'block');
+    } else if (sizeVal === "large") {
+      $("#large").css('display', 'block');
+    }
+  });
+
+  $("input:checkbox[name=toppings]").change(function() {
+    var toppingId =$("input:checkbox[name=toppings]:checked").val();
+
+    if (toppingId === "broccoli") {
+      $("#broccoli").removeClass("hidden")
+    } else if (toppingId === "cheese") {
+      $("#cheese").removeClass("hidden")
+    } else if (toppingId === "olives") {
+      $("#olives").removeClass("hidden")
+    } else if (toppingId === "pepperoni") {
+      $("#pepperoni").removeClass("hidden")
+    } else if (toppingId === "peppers") {
+      $("#peppers").removeClass("hidden")
+    } else if (toppingId === "sausage") {
+      $("#sausage").removeClass("hidden")
+    } else if (toppingId === "spinach") {
+      $("#spinach").removeClass("hidden")
+    }
+  })
+
   $("form#pizza-order").submit(function(event) {
     event.preventDefault();
 
@@ -46,6 +86,9 @@ $(document).ready(function () {
 
     });
 
+    $(".total-price").removeClass("hidden")
     $("#total").text(pizzaOrder.totalPrice())
+
+    pizzaOrder.resetForm();
   })
 })
